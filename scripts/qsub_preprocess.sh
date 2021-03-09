13703067663ab43cfa5e8e047b4c2c36a0ee317a
Edwin Solares <solarese@uci.edu>
2021-03-08 16:07:17 -0800

    Create qsub_preprocess.sh
    qsub version of sbatch_preprocess.sh

---
+++ scripts/qsub_preprocess.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+#$ -N mypreprocessrun
+#$ -t 1-1
+#$ -pe openmp 2
+#$ -R Y
+#$ -ckpt blcr
+#$ -q myqueue
+
+module load blat
+
+REF=myassemblyfile.fasta
+
+#create this file: faToTwoBit ${REF} $(basename ${REF} .fasta).2bit
+faToTwoBit ${REF} $(basename ${REF} .fasta).2bit
+
+python preprocessfasta.py -i ${REF}
+
+JOBFILE=jobfile.txt
+#ls contigs/*.fasta > ${JOBFILE}
+wait
