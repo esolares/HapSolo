82c79ac5d16bff858f02f42d3d30ae0d4f5bc35a
Edwin Solares <solarese@uci.edu>
2021-03-22 08:52:52 -0700

    Update bash_buscopreprocess.sh

--- scripts/bash_buscopreprocess.sh
+++ scripts/bash_buscopreprocess.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 
+REF=YOURASM.FASTA
+
+# make sure to start from your root working directory that contains your assmebly
+python preprocessfasta.py -i ${REF} -m 0
+
+cd contigs
 # make sure to run in ${WORKDIR}/contigs/ directory
 ls *.fasta > buscojobfile.txt
